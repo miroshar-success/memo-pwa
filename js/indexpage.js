c091bdb8748e25c21373c2d802a6d372e131c150
lukecam95 <67231783+lukecam95@users.noreply.github.com>
2022-05-25 18:47:12 +0200

    Update indexpage.js

--- js/indexpage.js
+++ js/indexpage.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
 });
 
 function getDeviceInfo() {
-	var sendPath = "/devInfo";
+	var sendPath = "http://192.168.4.1/devInfo";
 	var xhttp = new XMLHttpRequest();
 	xhttp.open("GET", sendPath);
 	xhttp.responseType = 'text';
